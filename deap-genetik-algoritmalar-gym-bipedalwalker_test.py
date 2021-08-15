b62eff3b10d625b5b5c55ba52d64a5ba08fcf985
fmehmetun <fmehmetun3@gmail.com>
2021-08-15 09:28:01 +0300

    minor change in CartPole, running environment before GA simulation; gym-BipedalWalker python scripts added, training now B|

---
+++ deap-genetik-algoritmalar-gym-bipedalwalker_test.py
@@ -0,0 +1,94 @@
+import random, gym
+import numpy as np
+from deap import algorithms, base, creator, tools
+
+modelInPath = "models/bipedalWalkerBest"
+
+# Kaydedilen modeli yükle
+best = np.load(modelInPath if modelInPath.endswith(".npy") else (modelInPath+".npy"))
+
+## Simülasyon ortamı
+env = gym.make("BipedalWalker-v3")
+observation_space_dim = env.observation_space.shape[0]
+action_space_dim = env.action_space.shape[0]
+
+# Tekrarlanabilirlik için seed ayarlanır
+SEED_VALUE = 64
+random.seed(SEED_VALUE)
+np.random.seed(SEED_VALUE)
+env.seed(SEED_VALUE)
+
+# Simülasyonu çalıştırmak için ortak fonksiyon
+def run_env(act_fn, n_episode=1, render=False):
+	# Episode döngüsü
+	totalRewards = 0
+	for episode in range(n_episode):
+		state = env.reset()
+		done = False
+
+		# Timestep döngüsü
+		while not done:
+			if render: env.render()
+
+			# act_fn adlı fonksiyon parametreyi çağırarak aksiyonu al
+			action = act_fn(state)
+			next_state, reward, done, info = env.step(action)
+
+			totalRewards += reward
+			state = next_state
+	return totalRewards
+
+## Yapay sinir ağı
+# Ara katman ünite sayısı
+nn_hidden_unit = 8
+w1_ndim = (observation_space_dim*nn_hidden_unit)
+w2_ndim = (nn_hidden_unit*action_space_dim)
+w3_ndim = (nn_hidden_unit*action_space_dim)
+
+# Verilen bireyin genotipini yapay sinir ağı parametreleri olarak kullanarak, state vektörünü feed-forward eder
+def nn_forward(individual, state):
+	# State'in 2 seviyeli array olduğundan emin ol: [?, observation_space_dim]
+	if len(state.shape) == 1: state = np.array([state])
+	assert len(state.shape) == 2
+
+	# Yapay sinir ağı parametrelerini genotip olan 1D vektörden çıkar
+	arr = np.array(individual)
+	w1 = arr[:w1_ndim]
+	b1 = arr[w1_ndim]
+	w_mu = arr[w1_ndim+1 : w1_ndim+1+w2_ndim]
+	b_mu = arr[w1_ndim+1+w2_ndim]
+	w_sigma = arr[w1_ndim+1+w2_ndim+1:-1]
+	b_sigma = arr[-1]
+
+	# Ağırlıkları matris çarpımı için yeniden şekillendir
+	w1 = np.reshape(w1, (observation_space_dim, nn_hidden_unit))
+	w_mu = np.reshape(w_mu, (nn_hidden_unit, action_space_dim))
+	w_sigma = np.reshape(w_sigma, (nn_hidden_unit, action_space_dim))
+
+	# Sigmoid fonksiyonu
+	sigmoid = lambda x: (1 / (1 + np.exp(-x)))
+	# ReLu fonksiyonu
+	relu = lambda x: np.maximum(0, x)
+	# Softplus fonksiyonu
+	softplus = lambda x: np.log1p(np.exp(x))
+	# Tanh fonksiyonu
+	tanh = lambda x: (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+	# Feed-forward
+	h1 = sigmoid(np.dot(state, w1) + b1)
+
+	# Normal dağılım için Mu & Sigma çıktıları
+	mu_output = tanh(np.dot(h1, w_mu) + b_mu)
+	sigma_output = relu(np.dot(h1, w_sigma) + b_sigma)
+
+	# Normal dağılımdan sample alarak aksiyonu döndür
+	output = np.random.normal(loc=mu_output[0], scale=sigma_output[0])
+	return output
+
+# En iyi bireyle simülasyonu çalıştır
+run_env(
+	act_fn=(lambda state: nn_forward(best, state)),
+	n_episode=10,
+	render=True
+)
+env.close()
--- deap-genetik-algoritmalar-gym-bipedalwalker_train.py
+++ deap-genetik-algoritmalar-gym-bipedalwalker_train.py
@@ -2,10 +2,12 @@ import random, gym
 import numpy as np
 from deap import algorithms, base, creator, tools
 
+modelOutPath = "models/bipedalWalkerBest"
+
 ## Simülasyon ortamı
-env = gym.make("CartPole-v1")
+env = gym.make("BipedalWalker-v3")
 observation_space_dim = env.observation_space.shape[0]
-action_space_dim = env.action_space.n
+action_space_dim = env.action_space.shape[0]
 
 # Tekrarlanabilirlik için seed ayarlanır
 SEED_VALUE = 64
@@ -38,39 +40,54 @@ def run_env(act_fn, n_episode=1, render=False):
 nn_hidden_unit = 8
 w1_ndim = (observation_space_dim*nn_hidden_unit)
 w2_ndim = (nn_hidden_unit*action_space_dim)
+w3_ndim = (nn_hidden_unit*action_space_dim)
 
-# Verilen bireyin genotipini yapay sinir ağı parametreleri olarak kullanarak, state vektörünü ağa feed-forward eder
+# Verilen bireyin genotipini yapay sinir ağı parametreleri olarak kullanarak, state vektörünü feed-forward eder
 def nn_forward(individual, state):
 	# State'in 2 seviyeli array olduğundan emin ol: [?, observation_space_dim]
 	if len(state.shape) == 1: state = np.array([state])
 	assert len(state.shape) == 2
 
 	# Yapay sinir ağı parametrelerini genotip olan 1D vektörden çıkar
-	individual = np.array(individual)
-	w1 = individual[:w1_ndim]
-	b1 = individual[w1_ndim]
-	w2 = individual[w1_ndim+1:-1]
-	b2 = individual[-1]
+	arr = np.array(individual)
+	w1 = arr[:w1_ndim]
+	b1 = arr[w1_ndim]
+	w_mu = arr[w1_ndim+1 : w1_ndim+1+w2_ndim]
+	b_mu = arr[w1_ndim+1+w2_ndim]
+	w_sigma = arr[w1_ndim+1+w2_ndim+1:-1]
+	b_sigma = arr[-1]
 
 	# Ağırlıkları matris çarpımı için yeniden şekillendir
 	w1 = np.reshape(w1, (observation_space_dim, nn_hidden_unit))
-	w2 = np.reshape(w2, (nn_hidden_unit, action_space_dim))
+	w_mu = np.reshape(w_mu, (nn_hidden_unit, action_space_dim))
+	w_sigma = np.reshape(w_sigma, (nn_hidden_unit, action_space_dim))
 
 	# Sigmoid fonksiyonu
 	sigmoid = lambda x: (1 / (1 + np.exp(-x)))
+	# ReLu fonksiyonu
+	relu = lambda x: np.maximum(0, x)
+	# Softplus fonksiyonu
+	softplus = lambda x: np.log1p(np.exp(x))
+	# Tanh fonksiyonu
+	tanh = lambda x: (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
 	# Feed-forward
 	h1 = sigmoid(np.dot(state, w1) + b1)
-	output = sigmoid(np.dot(h1, w2) + b2)
-	return np.argmax(output)
 
-## Genetik algoritmalar
+	# Normal dağılım için Mu & Sigma çıktıları
+	mu_output = tanh(np.dot(h1, w_mu) + b_mu)
+	sigma_output = relu(np.dot(h1, w_sigma) + b_sigma)
+
+	# Normal dağılımdan sample alarak aksiyonu döndür
+	output = np.random.normal(loc=mu_output[0], scale=sigma_output[0])
+	return output
+
 ## Hiperparametreler
 # Her bireyin gen uzunluğu (ağın parametre sayısı)
-n_features = (w1_ndim + w2_ndim + 2)
+n_features = (w1_ndim + w2_ndim + w3_ndim + 3)
 
 # Simüle edilecek nesil sayısı
-n_generation = 25
+n_generation = 1
 
 # Popülasyon boyutu (birey sayısı)
 n_population = 32
@@ -97,8 +114,8 @@ def calculateFitness(individual):
 	individualEpisodeRewards = run_env(
 		# Aksiyon fonksiyonumuz, bireyin genotipini kullanarak yapay sinir ağını çalıştırır
 		act_fn=(lambda state: nn_forward(individual, state)),
-		# Her uygunluk hesabında ortamı 3 episode çalıştır
-		n_episode=3
+		# Her uygunluk hesabında ortamı 1 episode çalıştır
+		n_episode=1
 	)
 	# tuple tipinde çevirmeli
 	return (individualEpisodeRewards, )
@@ -108,7 +125,7 @@ toolbox.register("evaluate", calculateFitness)
 
 # Hangi çaprazlama, mutasyon ve seçilim yöntemlerinin kullanılacağını tanımla
 toolbox.register("mate", tools.cxUniform, indpb=0.50)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.05)
+toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=2, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=selectionTournamentSize)
 
 # Popülasyonu oluştur
@@ -122,7 +139,7 @@ run_env(
 		random.choice(population),
 		state
 	)),
-	n_episode=10,
+	n_episode=5,
 	render=True
 )
 
@@ -142,10 +159,17 @@ best = hallOfFame[0]
 print("[+] En iyi uygunluk değeri: {}".format(best.fitness.values[0]))
 print("Genotip: ", best)
 
+# Simülasyon sonucu ortaya çıkan en iyi bireyi kaydet
+try:
+	np.save(modelOutPath, np.array(best))
+	print("[+] Model başarıyla kaydedildi!")
+except Exception as e:
+	print("[!] Model kaydedilemedi: {}".format(e))
+
 # En iyi bireyle simülasyonu çalıştır
 run_env(
 	act_fn=(lambda state: nn_forward(best, state)),
-	n_episode=5,
+	n_episode=10,
 	render=True
 )
 env.close()
